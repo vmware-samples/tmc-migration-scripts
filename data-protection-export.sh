9929468bdd5b65d2b5004e142040ad1e0bc06a54
hx036535 <hongsheng.xie@broadcom.com>
2025-06-20 11:12:46 +0800

    data-protection (#1)
    * data-protection
    
    Signed-off-by: Hongsheng Xie <hongsheng.xie@broadcom.com>
    
    * data-protection
    
    Signed-off-by: Hongsheng Xie <hongsheng.xie@broadcom.com>
    
    ---------
    
    Signed-off-by: Hongsheng Xie <hongsheng.xie@broadcom.com>

---
+++ data-protection-export.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+
+DPDIR=data-protection-data
+rm -fr ${DPDIR}
+mkdir -p ${DPDIR}
+
+# Save backup location for both org and clsuter
+echo "Saving backup-location ......"
+tanzu tmc data-protection backup-location list -o yaml -s org > ${DPDIR}/backup_location_org.yaml
+tanzu tmc data-protection backup-location list -o yaml -s cluster > ${DPDIR}/backup_location_cluster.yaml
+
+# Save schedule
+echo "Saving schedule ......"
+tanzu tmc data-protection schedule list -o yaml > ${DPDIR}/schedule.yaml
+
+# Save backup
+echo "Saving backup ......"
+tanzu tmc data-protection backup list -o yaml > ${DPDIR}/backup.yaml
+
+# Save restore
+echo "Saving restore ......"
+tanzu tmc data-protection restore list -o yaml > ${DPDIR}/restore.yaml
+
+# The others, these commands support only get/list
+echo "Saving others ......"
+tanzu tmc data-protection snapshot-location list -o yaml > ${DPDIR}/snapshot_location.yaml
+tanzu tmc data-protection template list | while read -r line; do
+    templ="${line%% *}"
+    if [[ "${templ}" != "NAME" ]]; then
+        tanzu tmc data-protection template get ${templ} > ${DPDIR}/template_${templ}.yaml
+    fi
+done
+
+# dataprotection on clusters/clustergroups
+#TMC_ACCESS_TOKEN=<required>
+#TMC_SAAS_ENDPOINT=<required>
+
+echo "Saving dataprotection for clustergroups ......"
+yq -r '.backupLocations[] | .spec.assignedGroups[] | .clustergroup.name' ${DPDIR}/backup_location_org.yaml | while read -r groupname; do
+    echo "    clustergroup: ${groupname}"
+    dpgrp=$(curl -k --http1.1 \
+            -H "Authorization: Bearer $TMC_ACCESS_TOKEN" \
+            -H "Content-Type: application/json" \
+            -X GET \
+	    ${TMC_SAAS_ENDPOINT}/v1alpha1/clustergroups/${groupname}/dataprotection)
+    if [[ "${dpgrp}" != "{}" ]]; then
+        echo ${dpgrp} | yq -y '.dataProtections' >> ${DPDIR}/dataprotection_clustergroups.yaml
+    fi
+done
+
+echo "Saving dataprotection for clusters ......"
+yq -r '.backupLocations[] | .fullName | .managementClusterName + " " + .provisionerName + " " + .clusterName' ${DPDIR}/backup_location_cluster.yaml | while read -r mgmtname provname clname; do
+    echo "    cluster: ${clname}"
+    dpcl=$(curl -k --http1.1 \
+            -H "Authorization: Bearer $TMC_ACCESS_TOKEN" \
+            -H "Content-Type: application/json" \
+            -X GET \
+            ${TMC_SAAS_ENDPOINT}/v1alpha1/clusters/${clname}/dataprotection\?fullName.managementClusterName=${mgmtname}\&fullName.provisionerName=${provname})
+    if [[ "${dpgrp}" != "{}" ]]; then
+        echo ${dpcl} | yq -y '.dataProtections' >> ${DPDIR}/dataprotection_clusters.yaml
+    fi
+done
---
+++ data-protection-import.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -e
+
+ONBOARDED_CLUSTER_INDEX_FILE="clusters/onboared-clusters-name-index"
+TEMPFILE=_temp_dp_file_$(date +%s)
+DPDIR=data-protection-data
+if [ ! -d "${DPDIR}" ]; then
+    echo "Dir %{DPDIR} doesn't exist!"
+    exit 1
+fi
+
+TANZU=tanzu
+#DRYRUN="--dry-run"
+DRYRUN=
+
+function create_dp_resources() {
+    local filename=$1
+    local keyname=$2
+    local dpcmd=$3
+    local total=$(yq -r ".totalCount" ${DPDIR}/${filename})
+    local idx=0
+    if [[ "${total}" == "0" ]]; then
+        echo "==> No data in ${filename}"
+        return
+    fi
+    echo "************************************************************************"
+    echo "Start loading ${filename}, total number is ${total}"
+    echo "************************************************************************"
+    while true; do
+        echo "==> Loading ${idx}/${total} in ${filename} ......"
+        # Do we need to delete '.meta'?
+        local elem=$(yq -j ".${keyname}[${idx}]" ${DPDIR}/${filename} | yq -j -c 'del(.status)')
+        if [[ "${elem}" == "null" ]]; then
+            break
+        fi
+        echo ${elem} > ${TEMPFILE}
+        cat ${TEMPFILE}
+        # Don't stop even the command fails, creating duplicated resoure will be
+        # handled by TMCSM
+        ${TANZU} tmc data-protection ${dpcmd} create -f ${TEMPFILE} ${DRYRUN} || true
+        idx=$((idx+1))
+    done
+}
+
+function enable_dataprotection() {
+    local filename=$1
+    local scope=$2
+    local total=$(yq ". | length" ${DPDIR}/${filename})
+    local idx=0
+    if [[ "${total}" == "0" ]]; then
+        echo "==> No data in ${filename}"
+        return
+    fi
+    echo "************************************************************************"
+    echo "Start loading ${filename}, total number is ${total}"
+    echo "************************************************************************"
+    while true; do
+        if [[ $idx -ge $total ]]; then
+            break
+        fi
+        echo "==> Loading ${idx}/${total} in ${filename} ......"
+        # Do we need to delete '.meta'?
+        yq -y ".[${idx}]" ${DPDIR}/${filename} | yq -y 'del(.status)' > ${TEMPFILE}
+        cat ${TEMPFILE}
+        if [[ "${scope}" == "cluster" ]]; then
+            fullname=$(yq '.fullName | .managementClusterName + "." + .provisionerName + "." + clusterName' ${TEMPFILE})
+            if ! grep -qxf "${fullname}" "$ONBOARDED_CLUSTER_INDEX_FILE"; then
+                echo "Cluster ${fullname} doesn't exist, just skip it"
+                idx=$((idx+1))
+                continue
+            fi
+        fi
+        # Don't stop even the command fails, creating duplicated resoure will be
+        # handled by TMCSM
+        ${TANZU} tmc data-protection enable -s ${scope} -f ${TEMPFILE} ${DRYRUN} || true
+        idx=$((idx+1))
+    done
+}
+
+# Load backup location for both org and clsuter
+create_dp_resources "backup_location_org.yaml" "backupLocations" "backup-location"
+create_dp_resources "backup_location_cluster.yaml" "backupLocations" "backup-location"
+
+# Load schedule
+create_dp_resources "schedule.yaml" "schedules" "schedule"
+
+# Load backup
+create_dp_resources "backup.yaml" "backups" "backup"
+
+# Load restore
+create_dp_resources "restore.yaml" "restores" "restore"
+
+# Enable dataprotection on clustergroups and clusters
+enable_dataprotection "dataprotection_clustergroups.yaml" "clustergroup"
+enable_dataprotection "dataprotection_clusters.yaml" "cluster"
+
+rm -f ${TEMPFILE}
