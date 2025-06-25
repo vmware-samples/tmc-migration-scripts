dc82633dc506cb8c75f5a13a01f65c8ea2531a07
steven zou <steven.zou@broadcom.com>
2025-06-25 13:21:16 +0800

    Handle CLI init extra work

--- 001-base-saas_stack-connect.sh
+++ 001-base-saas_stack-connect.sh
@@ -2,14 +2,19 @@
 
 # export TANZU_API_TOKEN=<CSP-TOKEN>
 # export ORG_NAME=tanzumissioncontroluserorgstg
+# export TANZU_CLI_CEIP_OPT_IN_PROMPT_ANSWER=no
 
 
 ## Create CLI context.
 export TMC_CONTEXT=migration
 
+# Clear the context first if it exists.
+tanzu context delete ${TMC_CONTEXT} -y
+
 # For production.
 #tanzu context create ${TMC_CONTEXT} --type tmc --endpoint $ORG_NAME.tmc.tanzu.broadcom.com:443
 
+tanzu config eula accept
 # For experiments.
 tanzu context create ${TMC_CONTEXT} --type tmc --staging --endpoint $ORG_NAME.tmc-dev.tanzu.broadcom.com:443
 
