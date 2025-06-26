267731695837f777d4753959c94178873a9ab873
hx036535 <hongsheng.xie@broadcom.com>
2025-06-26 09:42:53 +0800

    Improve data-protection import (#47)
    Signed-off-by: Hongsheng Xie <hongsheng.xie@broadcom.com>

--- 027-cluster-data_protection-export.sh
+++ 027-cluster-data_protection-export.sh
@@ -12,9 +12,19 @@ tanzu tmc data-protection backup-location list -o yaml -s org > ${DPDIR}/backup_
 tanzu tmc data-protection backup-location list -o yaml -s cluster > ${DPDIR}/backup_location_cluster.yaml
 
 # Save schedule
-echo "Saving schedule ......"
-tanzu tmc data-protection schedule list -s cluster -o yaml > ${DPDIR}/schedule-clustergroup.yaml
-tanzu tmc data-protection schedule list -s clustergroup -o yaml > ${DPDIR}/schedule-cluster.yaml
+echo "Saving schedule for clusters ......"
+tanzu tmc data-protection schedule list -s cluster -o yaml > ${DPDIR}/schedule-cluster.yaml
+echo "Saving schedule for clustergroups ......"
+yq -r '.backupLocations[] | .spec.assignedGroups[] | select(.clustergroup) | .clustergroup.name' ${DPDIR}/backup_location_org.yaml | while read -r groupname; do
+    echo "    clustergroup: ${groupname}"
+    schd=$(tanzu tmc data-protection schedule list -s clustergroup --cluster-group-name ${groupname} -o json)
+    if [[ "${schd}" != "{}" ]]; then
+        schds=$(echo ${schd} | yq -o yaml -P '.schedules')
+        if [[ "${schds}" != "null" ]] && [[ "${schds}" != "[]" ]]; then
+            echo "${schds}" >> ${DPDIR}/schedule-clustergroup.yaml
+        fi
+    fi
+done
 
 # Save backup
 echo "Saving backup ......"
@@ -38,6 +48,10 @@ done
 #TMC_ACCESS_TOKEN=<required>
 #TMC_SAAS_ENDPOINT=<required>
 
+# For test only
+#TMC_ACCESS_TOKEN=$(curl -s -X POST -H "Content-Type=application/x-www-form-urlencoded" https://console-stg.tanzu.broadcom.com/csp/gateway/am/api/auth/api-tokens/authorize --data-urlencode "refresh_token=$MY_CSP_TOKEN" | jq -r '.access_token')
+#TMC_SAAS_ENDPOINT="https://trh.tmc-dev.tanzu.broadcom.com"
+
 echo "Saving dataprotection for clustergroups ......"
 yq -r '.backupLocations[] | .spec.assignedGroups[] | select(.clustergroup) | .clustergroup.name' ${DPDIR}/backup_location_org.yaml | while read -r groupname; do
     echo "    clustergroup: ${groupname}"
@@ -47,20 +61,23 @@ yq -r '.backupLocations[] | .spec.assignedGroups[] | select(.clustergroup) | .cl
             -X GET \
             ${TMC_SAAS_ENDPOINT}/v1alpha1/clustergroups/${groupname}/dataprotection)
     if [[ "${dpgrp}" != "{}" ]]; then
-        echo ${dpgrp} | yq -o yaml -P '.dataProtections' >> ${DPDIR}/dataprotection_clustergroups.yaml
+        dps=$(echo ${dpgrp} | yq -o yaml -P '.dataProtections')
+        if [[ "${dps}" != "null" ]] && [[ "${dps}" != "[]" ]]; then
+           echo "${dps}"  >> ${DPDIR}/dataprotection_clustergroups.yaml
+        fi
     fi
 done
 
 echo "Saving dataprotection for clusters ......"
-yq -r '.backupLocations[] | .fullName | .managementClusterName + " " + .provisionerName + " " + .clusterName' ${DPDIR}/backup_location_cluster.yaml | while read -r mgmtname provname clname; do
-#yq -r '.backupLocations[] | .spec.assignedGroups[] | select(.cluster) | .cluster.managementClusterName + " " + .cluster.provisionerName + " " + .cluster.name' ${DPDIR}/backup_location_org.yaml | while read -r mgmtname provname clname; do
+#yq -r '.backupLocations[] | .fullName | .managementClusterName + " " + .provisionerName + " " + .clusterName' ${DPDIR}/backup_location_cluster.yaml | while read -r mgmtname provname clname; do
+yq -r '.backupLocations[] | .spec.assignedGroups[] | select(.cluster) | .cluster.managementClusterName + " " + .cluster.provisionerName + " " + .cluster.name' ${DPDIR}/backup_location_org.yaml | while read -r mgmtname provname clname; do
     echo "    cluster: ${clname}"
     dpcl=$(curl -k -s --http1.1 \
             -H "Authorization: Bearer $TMC_ACCESS_TOKEN" \
             -H "Content-Type: application/json" \
             -X GET \
             ${TMC_SAAS_ENDPOINT}/v1alpha1/clusters/${clname}/dataprotection\?fullName.managementClusterName=${mgmtname}\&fullName.provisionerName=${provname})
-    if [[ "${dpgrp}" != "{}" ]]; then
+    if [[ "${dpcl}" != "{}" ]]; then
         echo ${dpcl} | yq -o yaml -P '.dataProtections' >> ${DPDIR}/dataprotection_clusters.yaml
     fi
 done
