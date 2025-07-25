4927678e057e21bfd68e8c430752ee189d51643f
steven zou <steven.zou@broadcom.com>
2025-07-25 14:50:34 +0800

    add notes for MFA enabled case

--- 033-base-sm_stack-connect.sh
+++ 033-base-sm_stack-connect.sh
@@ -5,8 +5,16 @@
 #export TMC_SELF_MANAGED_DNS=tmc.tanzu.io
 #export TMC_SM_CONTEXT=tmc-sm
 
+# If MFA is enabled for the IDP, export this environment variable.
+#export TMC_SM_IDP_MFA_ENABLED=true
+
 # Clear context first.
 tanzu context delete ${TMC_SM_CONTEXT}  -y
 
 # Create a context.
-tanzu tmc context create ${TMC_SM_CONTEXT} --endpoint ${TMC_SELF_MANAGED_DNS} -i pinniped --basic-auth
+if [[ $TMC_SM_IDP_MFA_ENABLED == "true" ]]; then
+    echo "The IDP MFA is enabled. Please follow the guide to open the URL in a browser and complete the authentication process."
+    tanzu tmc context create ${TMC_SM_CONTEXT} --endpoint ${TMC_SELF_MANAGED_DNS} -i pinniped
+else
+    tanzu tmc context create ${TMC_SM_CONTEXT} --endpoint ${TMC_SELF_MANAGED_DNS} -i pinniped --basic-auth
+fi
