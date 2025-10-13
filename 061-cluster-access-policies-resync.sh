c5da5399afe98a5e58eb7fb2456236d20da8dcce
Willis Ren <willis.ren@broadcom.com>
2025-10-13 15:14:11 +0800

    Add script to resync custom role and rolebindings after being imported
    Signed-off-by: Willis Ren <willis.ren@broadcom.com>

---
+++ 061-cluster-access-policies-resync.sh
@@ -0,0 +1,71 @@
+#! /bin/bash
+
+source utils/common.sh
+
+register_last_words "Resync rolebindings on clusters and namespaces"
+
+TMC_NAMESPACE="vmware-system-tmc"
+
+DATA_DIR="data"
+SRC_DIR="$DATA_DIR/policies/iam"
+TEMP_DIR="$PWD/$SRC_DIR/$(date +%s)"
+TEMPKUBECONFIG="$TEMP_DIR/kubeconfig"
+
+# Create temp directory
+mkdir -p "$TEMP_DIR"
+
+export KUBECONFIG=$TEMPKUBECONFIG
+
+log "************************************************************************"
+log "* Resync Rolebindings on Clusters and Namespaces ..."
+log "************************************************************************"
+
+if [[ -z "$ONBOARDED_CLUSTER_INDEX_FILE" ]]; then
+    log error "ONBOARDED_CLUSTER_INDEX_FILE is not set"
+    exit 1
+elif [[ ! -f "$ONBOARDED_CLUSTER_INDEX_FILE" ]]; then
+    log error "$ONBOARDED_CLUSTER_INDEX_FILE doesn't exist"
+    exit 1
+fi
+
+while IFS="." read -r mgmt prvn cls; do
+    log info "Connecting to the cluster $mgmt/$prvn/$cls..."
+    if ! tanzu tmc cluster kubeconfig get $cls -m $mgmt -p $prvn > ${TEMPKUBECONFIG} 2>/dev/null; then
+        log error "Failed to get kubeconfig for cluster $mgmt/$prvn/$cls"
+        exit 1
+    fi
+
+    # Verify cluster connection
+    if ! kubectl cluster-info >/dev/null 2>&1; then
+        log error "Failed to connect to cluster $mgmt/$prvn/$cls"
+        exit 1
+    fi
+
+    log info "Delete stale custom cluster roles for $mgmt/$prvn/$cls ..."
+    kubectl get customrole --no-headers -o custom-columns=":metadata.name" | while read -r role; do
+        kubectl delete clusterrole $role
+    done
+
+    org_id=$(kubectl -n $TMC_NAMESPACE get cm stack-config -o jsonpath='{.data.org_id}' 2>/dev/null)
+    if [[ -z "$org_id" ]]; then
+        log error "Failed to get org_id for the cluster $mgmt/$prvn/$cls"
+        exit 1
+    fi
+  
+    log info "Delete stale cluster rolebindings on $mgmt/$prvn/$cls ..."
+    kubectl get clusterrolebinding --no-headers -o custom-columns=":metadata.name" -A | while read -r binding; do
+        if [[ $binding =~ org-.*-rbac.authorization.k8s.io ]] && [[ ! $binding =~ $org_id ]]; then
+            kubectl delete clusterrolebinding $binding
+        fi
+    done
+
+    log info "Delete stale rolebindings for $mgmt/$prvn/$cls ..."
+    kubectl get rolebinding --no-headers -o custom-columns=":metadata.name,:metadata.namespace" | while read -r binding namespace; do
+        if [[ $binding =~ org-.*-rbac.authorization.k8s.io ]] && [[ ! $binding =~ $org_id ]]; then
+            kubectl delete rolebinding $binding -n $namespace
+        fi
+    done
+
+    log info "Restart policy sync extension to resync access policies ..."
+    kubectl rollout restart deployment policy-sync-extension -n $TMC_NAMESPACE
+done < $ONBOARDED_CLUSTER_INDEX_FILE
